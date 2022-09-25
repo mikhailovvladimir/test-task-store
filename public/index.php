b7fb2efa3a453deeebee4158cc705112ac8074c8
Vladimir <1602211@gmail.com>
2022-09-26 02:48:35 +0600

    added class Route and changed front controller

--- public/index.php
+++ public/index.php
@@ -1,37 +1,19 @@
 <?php
+spl_autoload_register(function (string $className) {
+    require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
+});
+
+use Core\Exceptions\NotFoundPage;
+use Core\RequestHandler\Route;
+use Core\View\Render;
+
 try {
-    spl_autoload_register(function (string $className) {
-        require_once __DIR__ . '/../src/' . str_replace('\\', '/', $className) . '.php';
-    });
-
-    $route = filter_input(INPUT_GET, 'route', FILTER_SANITIZE_STRING);
-
-    $routes = require __DIR__ . '/../src/Config/routes.php';
-
-    $isRouteFound = false;
-    foreach ($routes as $pattern => $controllerAndAction) {
-        preg_match($pattern, $route, $matches);
-        if (!empty($matches)) {
-            $isRouteFound = true;
-            break;
-        }
-    }
-
-    if (!$isRouteFound) {
-        throw new \Core\Exceptions\NotFoundPage();
-    }
-
-    unset($matches[0]);
-
-    $controllerName = $controllerAndAction[0];
-    $actionName = $controllerAndAction[1];
-
-    $controller = new $controllerName();
-    $controller->$actionName(...$matches);
-} catch (\Core\Exceptions\NotFoundPage $e) {
-    $view = new Core\View\Render(__DIR__ . '/../src/TestTask/View/');
+    $route = new Route();
+    $route->start();
+} catch (NotFoundPage $e) {
+    $view = new Render(__DIR__ . '/../src/TestTask/View/');
     $view->getHtmlPage('errors/404.php', ['error' => $e->getMessage()], 404);
-} catch (\Throwable $e) {
-    $view = new Core\View\Render(__DIR__ . '/../src/TestTask/View/');
+} catch (Throwable $e) {
+    $view = new Render(__DIR__ . '/../src/TestTask/View/');
     $view->getHtmlPage('errors/500.php', ['error' => $e->getMessage()], 500);
 }
